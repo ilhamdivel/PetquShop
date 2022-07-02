b43a9536855d69bc14edceb147046410e1830aa0
Ilham <muhammadilhamhidayat3@gmail.com>
2022-07-02 13:18:03 +0700

    first commit

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","petqushop");
+
+if(!$conn){
+	echo "gagal konek database menn";
+} else {
+	
+};
+
+?>
