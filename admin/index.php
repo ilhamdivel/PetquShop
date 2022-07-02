133d8d69fd25e7bd994e72c5d053f0208a68ef72
Ilham <muhammadilhamhidayat3@gmail.com>
2022-07-02 15:42:47 +0700

    delete unused file and some changes

--- admin/index.php
+++ admin/index.php
@@ -188,29 +188,6 @@
                                 </div>
                             </div>
                         </div>
-						<div class="col-md-4">
-                            <div class="single-report mb-xs-30">
-                                <div class="s-report-inner pr--20 pt--30 mb-3">
-                                    <div class="icon"><i class="fa fa-link"></i></div>
-                                    <div class="s-report-title d-flex justify-content-between">
-                                        <h4 class="header-title mb-0">Konfirmasi Pembayaran</h4>
-                                    </div>
-                                    <div class="d-flex justify-content-between pb-2">
-                                        <h1><?php echo $itungtrans3 ?></h1>
-                                    </div>
-									<!--
-									<button type="button" class="<?php 
-									if($itungtrans3==0){
-										echo 'btn btn-secondary btn-block';
-									} else {
-										echo 'btn btn-primary btn-block';
-									}
-									?>
-									">Lihat Transaksi</button>
-									-->
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 
